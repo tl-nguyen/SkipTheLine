a2b2ce7cd4b98b21356985440835c25ea82867ad
Dobromir Brezoev <d.brezoev@gmail.com>
2014-09-29 04:21:45 -0500

    Filtering object in radius, implemented

--- SkipTheLine/scripts/app/home.js
+++ SkipTheLine/scripts/app/home.js
@@ -11,8 +11,22 @@ app.home = (function () {
             return 'Hello ' + app.Users.currentUser.get('data').DisplayName;
         },
         onFindNearest: function () {
-            //TODO: List all places in radius 1km
-            alert('clicked');
+            //TODO: Create a view for filtered data
+            app.helper.resolveCurrentLocation().then(function (position) {                
+                var query = new Everlive.Query();
+                query.where().nearSphere('Location', [position.coords.longitude, position.coords.latitude], 10, 'km');
+                var data = app.everlive.data('Place');
+                data.get(query)
+                    .then(function (data) {
+                            //alert(JSON.stringify(data));
+                            console.log(JSON.stringify(data));
+                        },
+                        function (error) {
+                            alert(JSON.stringify(error));
+                        });
+            });
+
+
         }
     }
 }());
