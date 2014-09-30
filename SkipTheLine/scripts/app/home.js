19251710aba525be5b5b2ce9ce80fcbde639787b
Dobromir Brezoev <d.brezoev@gmail.com>
2014-09-30 03:39:22 -0500

    order picture fetching works properly

--- SkipTheLine/scripts/app/home.js
+++ SkipTheLine/scripts/app/home.js
@@ -2,6 +2,11 @@ var app = app || {};
 
 app.home = (function () {
     'use strict';
+    var DEFAULT_DISTANCE = 5,
+        DEFAULT_DISTANCE_MEASURE = 'km',
+        DESCRIPTION_MESSAGE = 'Time is precious, skip the line ',
+        DEFAULT_LOGO_PATH = './styles/images/logo.jpg',
+        IMAGE_ALT = 'Skip the line logo';
 
     var greeting = function () {
         return 'Hello ' + app.Users.currentUser.get('data').DisplayName;
@@ -12,7 +17,7 @@ app.home = (function () {
             .then(function (position) {
 
                 var query = new Everlive.Query();
-                query.where().nearSphere('Location', [position.coords.longitude, position.coords.latitude], 5, 'km');
+                query.where().nearSphere('Location', [position.coords.longitude, position.coords.latitude], DEFAULT_DISTANCE, DEFAULT_DISTANCE_MEASURE);
 
                 var data = app.everlive.data('Place');
 
@@ -43,9 +48,9 @@ app.home = (function () {
     };
 
     return {
-        description: 'Time is precious, skip the line ',
-        path: './styles/images/logo.jpg',
-        imageAlt: 'Skip the line logo',
+        description: DESCRIPTION_MESSAGE,
+        path: DEFAULT_LOGO_PATH,
+        imageAlt: IMAGE_ALT,
         nearestPlacesDataSource: null,
         greeting: greeting,
         onFindNearest: onFindNearest
